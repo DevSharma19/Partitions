7f9727c57074e51c619027e09f980937579a5a9a
Dev Sharma <devnavla@gmail.com>
2018-10-13 15:14:56 +0530

    Changed numOfPartitionAfterNum to overloaded function of NumOfPartitions

--- src/Logic.java
+++ src/Logic.java
@@ -1,54 +1,54 @@
 public class Logic {
-	
+
 	public static int numOfPartitions(int num) {
 		int numOfPartitions = 0;
-		
+
 		for(int i = num; i > 0; i--) {
-			
+
 			int difference = num - i;
-			
+
 			if(difference <= i) {
-				
+
 				if(difference <= 1) {
-					
+
 					numOfPartitions++;
 				} else {
-					
+
 					numOfPartitions += numOfPartitions(difference);
 				}
 			} else {
-				
-				numOfPartitions += numOfPartitionsAfterNum(difference, i);
+
+				numOfPartitions += numOfPartitions(difference, i);
 			}
 		}
-		
+
 		return numOfPartitions;
 	}
-	
-	private static int numOfPartitionsAfterNum(int num, int realNum) {
-		
+
+	private static int numOfPartitions(int num, int realNum) {
+
 		int numOfPartitions = 0;
-		
+
 		for(int i = realNum; i > 0; i--) {
-			
+
 			int difference = num - i;
-			
+
 			if(difference <= i) {
-				
-				
+
+
 				if(difference <= 1) {
-					
+
 					numOfPartitions++;
 				} else {
-					
+
 					numOfPartitions += numOfPartitions(difference);
 				}
 			} else {
-				
-				numOfPartitions += numOfPartitionsAfterNum(difference, i);
+
+				numOfPartitions += numOfPartitions(difference, i);
 			}
 		}
-		
+
 		return numOfPartitions;
 	}
 }
