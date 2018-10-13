87b1c7eb763ed74db3c71970907d832c1e0cc006
Dev Sharma <devnavla@gmail.com>
2018-10-13 18:23:54 +0530

    1st try to list partitions

--- src/Driver.java
+++ src/Driver.java
@@ -5,8 +5,22 @@ public class Driver {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int num = sc.nextInt();
-		System.out.println(num + " has " + Logic.numOfPartitions(num) + " partitions");
+		System.out.println("Enter a number");
+		int choice = sc.nextInt();
+		
+		int numOfPartitions = Logic.numOfPartitions(choice);
+		System.out.println(choice + " has " + numOfPartitions + " partitions");
+		
+		int[][] partitions = Logic.partitions(choice);
+		for (int[] i : partitions) {
+			
+			System.out.println();
+			for (int j : i) {
+				
+				if(j != 0)
+					System.out.print(j + ", ");
+			}
+		}
 		
 		sc.close();
 	}
--- src/Logic.java
+++ src/Logic.java
@@ -2,52 +2,126 @@ public class Logic {
 
 	public static int numOfPartitions(int num) {
 		int numOfPartitions = 0;
-
+		
 		for(int i = num; i > 0; i--) {
-
+			
 			int difference = num - i;
-
+			
 			if(difference <= i) {
-
+				
 				if(difference <= 1) {
-
+					
 					numOfPartitions++;
 				} else {
-
+					
 					numOfPartitions += numOfPartitions(difference);
 				}
 			} else {
-
+				
 				numOfPartitions += numOfPartitions(difference, i);
 			}
 		}
-
+		
 		return numOfPartitions;
 	}
-
+	
 	private static int numOfPartitions(int realNum, int num) {
-
 		int numOfPartitions = 0;
-
+		
 		for(int i = num; i > 0; i--) {
-
+			
 			int difference = realNum - i;
-
+			
 			if(difference <= i) {
-
+				
 				if(difference <= 1) {
-
+					
 					numOfPartitions++;
 				} else {
-
+					
 					numOfPartitions += numOfPartitions(difference);
 				}
 			} else {
-
+				
 				numOfPartitions += numOfPartitions(difference, i);
 			}
 		}
-
+		
 		return numOfPartitions;
+	}
+	
+	public static int[][] partitions(int num) {
+		int[][] partitions = new int[numOfPartitions(num)][num];
+		
+		for(int numCopy = num, inc = 0; numCopy > 0 && inc < partitions.length; numCopy--, inc++) {
+			
+			int difference = num - numCopy;
+			
+			if(difference <= numCopy) {
+				
+				if(difference <= 1) {
+					
+					partitions[inc][0] = numCopy;
+					
+					if(difference != 0)
+						partitions[inc][1] = difference;
+				} else {
+					
+					int[][] partitionsOfDifference = partitions(difference);
+					for(int[] i : partitionsOfDifference) {
+						
+						partitions[inc][0] = numCopy;
+						for(int j = 1; j <= i.length; j++) {
+							
+							partitions[inc][j] = i[j - 1];
+						}
+						
+						inc++;
+					}
+				}
+			} else {
+				
+				int[][] partitionsOfDifference = partitions(difference, numCopy);
+			}
+		}
+		
+		return partitions;
+	}
+	
+	private static int[][] partitions(int realNum, int num) {
+		int[][] partitions = new int[numOfPartitions(realNum) - 1][realNum];
+		
+		for(int numCopy = num, inc = 0; numCopy > 0; numCopy--, inc++) {
+			
+			int difference = realNum - numCopy;
+			
+			if(difference <= numCopy) {
+				
+				if(difference <= 1) {
+					
+					partitions[inc][0] = numCopy;
+					
+					if(difference != 0)
+						partitions[inc][1] = difference;
+				} else {
+					
+					int[][] partitionsOfDifference = partitions(difference);
+					for(int[] i : partitionsOfDifference) {
+						
+						partitions[inc][0] = numCopy;
+						for(int j = 1; j <= i.length; j++) {
+							partitions[inc][j] = i[j - 1];
+						}
+						inc++;
+					}
+				}
+			} else {
+				
+
+				int[][] partitionsOfDifference = partitions(difference, numCopy);
+			}
+		}
+		
+		return partitions;
 	}
 }
